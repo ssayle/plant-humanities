0c27e74bc575373e0b9a3125b68e46bce69c82d3
Ron Snyder <ron@snyderjr.com>
2024-02-05 13:50:47 -0500

    testing

--- juncture/index.js
+++ juncture/index.js
@@ -7,7 +7,7 @@ const junctureDependencies = [
   {tag: 'script', src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js'},
   {tag: 'script', src: 'https://cdnjs.cloudflare.com/ajax/libs/tippy.js/6.3.7/tippy.umd.min.js'},
   {tag: 'script', src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js'},
-  {tag: 'script', src: '../v2/dist/js/index.js', type: 'module'}
+  {tag: 'script', src: '../juncture/v2/dist/js/index.js', type: 'module'}
 ]
 
 const isJunctureV1 = Array.from(document.querySelectorAll('param'))

aee0cf6e3adb8437a01faebe485f0a0a4071f4d7
Ron Snyder <ron@snyderjr.com>
2024-02-10 14:26:11 -0500

    scriptBasePath path fix

--- ezsite/juncture/index.js
+++ ezsite/juncture/index.js
@@ -1,7 +1,10 @@
 window.config.scriptBasePath = Array.from(document.querySelectorAll('script'))
 .filter(script => script.src)
 .filter(script => /\/ezsite\/index\.js$/.test(script.src))
-.map(scriptEl => `/${new URL(scriptEl.src).pathname.split('/').filter(pe => pe).slice(0, -2).join('/')}`)
+.map(scriptEl => {
+  let path = new URL(scriptEl.src).pathname.split('/').filter(pe => pe)
+  return path.length > 0 ? `/${path.join('/')}` : ''
+})
 ?.[0] || ''
 
 const junctureDependencies = [

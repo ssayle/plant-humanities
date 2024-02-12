6c01a7dec62ec67697ad84147487f13b2b579dbe
Ron Snyder <ron@snyderjr.com>
2024-02-12 14:32:19 -0500

    Fix path for curtain-sync viewer

--- ezsite/juncture/v1/Compare.vue
+++ ezsite/juncture/v1/Compare.vue
@@ -32,7 +32,7 @@ module.exports = {
     viewerIcon: 'fas fa-images',
     dependencies: [
       'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js',
-      '/juncture/v1/openseadragon-curtain-sync.js'
+      `${window.config.scriptBasePath}/ezsite/juncture/v1/openseadragon-curtain-sync.js`
     ],
     viewer: null,
     manifests: null
